fb6e4c05c47a458bc2125327d30900dc280d0a91
SYSTEX\2400443 <2400443@systex.com.tw>
2024-06-20 18:15:30 +0800

    修正登入頁面密碼 encoder 模式與 mockserver config

--- demo/src/main/java/com/example/demo/config/DemoSecurityConfig.java
+++ demo/src/main/java/com/example/demo/config/DemoSecurityConfig.java
@@ -9,16 +9,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Configuration
 public class DemoSecurityConfig {
 
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
     //authenticationProvider bean definition
     @Bean
     public DaoAuthenticationProvider authenticationProvider(UserService userService) {
         DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
         auth.setUserDetailsService(userService); //set the custom user details service
-        auth.setPasswordEncoder(passwordEncoder()); //set the password encoder - bcrypt
         return auth;
     }
 }
